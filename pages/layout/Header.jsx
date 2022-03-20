e69517ad68acba2cd0622b48f3870640968d61ed
chulocr8v <nkematu5@gmail.com>
2022-03-20 18:24:34 +0100

    updates

--- pages/layout/Header.jsx
+++ pages/layout/Header.jsx
@@ -5,11 +5,33 @@ import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [hideHeader, sethideHeader] = useState(false);
+  const [scrollY, setScrollY] = useState('window.pageYOffset' );
+  
+    let prevPos = scrollY
+    const header = () => {
+      let currentPos = window.pageYOffset 
+      if(currentPos > prevPos) {
+        sethideHeader(true)
+      } else {
+        sethideHeader(false)
+      }
+        prevPos = currentPos
+    }
+ 
+ useEffect(()=> {
+   window.addEventListener('scroll', header);
+   return () => {
+    window.removeEventListener('scroll', header)
+   } 
+ }, [])
+  
   return (
-    <header className={styles.header}>
+    <header className={styles.header} id={hideHeader && styles.hide_header} >
       <div className={styles.container}>
         <div className={styles.logo}>Logo</div>
         <FaBars
+          id = {showMenu && styles.rotate} 
           className={styles.icon}
           onClick={() => {
             setShowMenu(!showMenu);
